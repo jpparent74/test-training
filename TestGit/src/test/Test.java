62a68a3177486d9c56c2f6b3fb52052585359ebc
JP Parent <jpparent@afi-sa.fr>
2023-12-07 08:17:33 +0100

    Refacto ajout printAjout

--- TestGit/src/test/Test.java
+++ TestGit/src/test/Test.java
@@ -3,6 +3,10 @@ package test;
 
 public class Test {
 	public static void main(String[] args) {
+		printAjout();
+	}
+	
+	private static void printAjout() {
 		System.out.println("Ajout printl");
 	}
 }
